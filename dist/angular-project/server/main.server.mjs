22b3b838d9ee051723856dc0eb168fe8c6f73ce3
Luseramia <108973001+Luseramia@users.noreply.github.com>
2024-11-15 06:03:57 +0700

    test1

--- dist/angular-project/server/main.server.mjs
+++ dist/angular-project/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YHSUIU3R.mjs";import"./chunk-RBU7GDRZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-TNXIPN6Z.mjs";import"./chunk-RBU7GDRZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
